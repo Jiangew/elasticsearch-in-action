e31d1d71c5d412a64b0aac24b3ba44cfab5e1cfc
Jiangew <jiangyixiang2010@sina.cn>
2017-09-14 19:06:34 +0800

    init elasticsearch-in-action proj, add client junit 5 test

---
+++ client/src/main/java/me/jiangew/elasticsearch/client/Builder.java
@@ -0,0 +1,122 @@
+package me.jiangew.elasticsearch.client;
+
+import org.apache.http.Header;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.http.message.BasicHeader;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+
+/**
+ * Author: Jiangew
+ * Date: 14/09/2017
+ */
+public class Builder {
+
+    public static RestClient client() {
+
+        return RestClient.builder(
+                new HttpHost("localhost", 9200, "http"),
+                new HttpHost("localhost", 9201, "http")
+        ).build();
+    }
+
+    public static RestClientBuilder builder() {
+        return RestClient.builder(new HttpHost("localhost", 9200, "http"));
+    }
+
+    /**
+     * Set the default headers that need to be sent with each request
+     *
+     * @param builder
+     * @return
+     */
+    public static RestClientBuilder setDefaultHeaders(RestClientBuilder builder) {
+        Header[] defaultHeaders = new Header[]{new BasicHeader("Content-Type", "application/json")};
+        builder.setDefaultHeaders(defaultHeaders);
+
+        return builder;
+    }
+
+    /**
+     * Set the timeout that should be honoured in case multiple attempts are made for the same request
+     *
+     * @param builder
+     * @return
+     */
+    public static RestClientBuilder setMaxRetryTimeoutMillis(RestClientBuilder builder) {
+        return builder.setMaxRetryTimeoutMillis(10000);
+    }
+
+    /**
+     * Set a listener that gets notified every time a node fails, in case actions need to be taken.
+     *
+     * @param builder
+     * @return
+     */
+    public static RestClientBuilder setFailureListener(RestClientBuilder builder) {
+        return builder.setFailureListener(new RestClient.FailureListener() {
+            @Override
+            public void onFailure(HttpHost host) {
+
+            }
+        });
+    }
+
+    /**
+     * Set a callback that allows to modify the default request configuration
+     *
+     * @param builder
+     * @return
+     */
+    public static RestClientBuilder setRequestConfigCallback(RestClientBuilder builder) {
+        return builder.setRequestConfigCallback(new RestClientBuilder.RequestConfigCallback() {
+            @Override
+            public RequestConfig.Builder customizeRequestConfig(RequestConfig.Builder requestConfigBuilder) {
+                return requestConfigBuilder.setSocketTimeout(10000);
+            }
+        });
+    }
+
+    /**
+     * Set a callback that allows to modify the http client configuration
+     *
+     * @param builder
+     * @return
+     */
+    public static RestClientBuilder setHttpClientConfigCallback(RestClientBuilder builder) {
+        // Basic authentication
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("elastic", "elastic"));
+
+        // Set a callback that allows to modify the http client configuration
+        builder.setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+            @Override
+            public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+                // proxy
+//                httpClientBuilder.setProxy(new HttpHost("proxy", 9000, "http"));
+
+                // The Apache Http Async Client starts by default one dispatcher thread,
+                // and a number of worker threads used by the connection manager,
+                // as many as the number of locally detected processors (depending on what Runtime.getRuntime().availableProcessors() returns).
+//                httpClientBuilder.setDefaultIOReactorConfig(IOReactorConfig.custom().setIoThreadCount(1).build());
+
+                // Preemptive Authentication can be disabled,
+                // which means that every request will be sent without authorization headers to see if it is accepted and,
+                // upon receiving a HTTP 401 response, it will resend the exact same request with the basic authentication header.
+                httpClientBuilder.disableAuthCaching();
+                httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+
+                return httpClientBuilder;
+            }
+        });
+
+        return builder;
+    }
+
+}
---
+++ client/src/test/java/me/jiangew/elasticsearch/client/RestClientTests.java
@@ -0,0 +1,222 @@
+package me.jiangew.elasticsearch.client;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpEntity;
+import org.apache.http.entity.ContentType;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.nio.entity.NStringEntity;
+import org.apache.http.util.EntityUtils;
+import org.elasticsearch.client.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Author: Jiangew
+ * Date: 14/09/2017
+ */
+public class RestClientTests {
+    private Log log = LogFactory.getLog(RestClientTests.class);
+
+    private Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
+
+    private Map<String, String> emptyParams = Collections.emptyMap();
+    private Map<String, String> singletonParams = Collections.singletonMap("pretty", "true");
+
+    // Define what needs to happen when the request is successfully performed
+    // Define what needs to happen when the request fails, meaning whenever there’s a connection error or a response with error status code is returned.
+    private ResponseListener responseListener = new ResponseListener() {
+        @Override
+        public void onSuccess(Response response) {
+            try {
+                handleResponse(response);
+            } catch (Exception e) {
+                log.error(e);
+            }
+        }
+
+        @Override
+        public void onFailure(Exception e) {
+            log.error(e);
+        }
+    };
+
+    // Controls how the response body gets streamed from a non-blocking HTTP connection on the client side.
+    // When not provided, the default implementation is used which buffers the whole response body in heap memory, up to 100 MB.
+    private HttpAsyncResponseConsumerFactory.HeapBufferedResponseConsumerFactory consumerFactory = new HttpAsyncResponseConsumerFactory.HeapBufferedResponseConsumerFactory(30 * 1024 * 1024);
+
+    private RestClientBuilder builder = Builder.builder();
+
+    private void handleResponse(Response response) throws Exception {
+        int statusCode = response.getStatusLine().getStatusCode();
+        String responseBody = EntityUtils.toString(response.getEntity());
+
+        assertEquals(200, statusCode);
+        log.info(responseBody);
+    }
+
+    private void handleRequestAsync() throws Exception {
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("user", "JamesiWork");
+        jsonObject.addProperty("postDate", "2013-01-30");
+        jsonObject.addProperty("message", "trying out Elasticsearch");
+        HttpEntity entity = new NStringEntity(jsonObject.toString(), ContentType.APPLICATION_JSON);
+
+        // rest client
+        RestClient restClient = Builder.builder().build();
+
+        final CountDownLatch latch = new CountDownLatch(5);
+        for (int i = 0; i < 5; i++) {
+            restClient.performRequestAsync(
+                    "PUT",
+                    "/twitter/doc" + 10 + i,
+                    Collections.<String, String>emptyMap(),
+                    //let's assume that the documents are stored in an HttpEntity array
+                    entity,
+                    new ResponseListener() {
+                        @Override
+                        public void onSuccess(Response response) {
+                            // Process the returned response
+                            latch.countDown();
+                        }
+
+                        @Override
+                        public void onFailure(Exception e) {
+                            // Handle the returned exception,
+                            // due to communication error or a response with status code that indicates an error
+                            latch.countDown();
+                        }
+                    }
+            );
+        }
+        latch.await();
+    }
+
+    @Test
+    void performRequestWithParams() throws Exception {
+        builder = Builder.setHttpClientConfigCallback(builder);
+        RestClient restClient = builder.build();
+        Response response = restClient.performRequest("GET", "/twitter/doc/1", singletonParams);
+        handleResponse(response);
+
+        restClient.close();
+    }
+
+    @Test
+    void performRequestWithParamsAsync() throws Exception {
+        builder = Builder.setHttpClientConfigCallback(builder);
+        RestClient restClient = builder.build();
+        restClient.performRequestAsync("GET", "/twitter/doc/1", singletonParams, responseListener);
+    }
+
+    @Test
+    void performRequestWithHeaders() throws Exception {
+        builder = Builder.setHttpClientConfigCallback(builder);
+        RestClient restClient = builder.build();
+        Response response = restClient.performRequest("GET", "/twitter/doc/1", singletonParams, new BasicHeader("Content-Type", "application/json"));
+        handleResponse(response);
+
+        restClient.close();
+    }
+
+    @Test
+    void performRequestWithHeadersAsync() throws Exception {
+        builder = Builder.setHttpClientConfigCallback(builder);
+        RestClient restClient = builder.build();
+        restClient.performRequestAsync("GET", "/twitter/doc/1", singletonParams, responseListener, new BasicHeader("Content-Type", "application/json"));
+    }
+
+    @Test
+    void performRequestWithEntity() throws Exception {
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("user", "JamesiWork");
+        jsonObject.addProperty("postDate", "2013-02-01");
+        jsonObject.addProperty("message", "ELK: Elasticsearch Logstash Kibana");
+        HttpEntity entity = new NStringEntity(jsonObject.toString(), ContentType.APPLICATION_JSON);
+
+        builder = Builder.setHttpClientConfigCallback(builder);
+        builder = Builder.setDefaultHeaders(builder);
+        RestClient restClient = builder.build();
+        Response response = restClient.performRequest("PUT", "/twitter/doc/6", singletonParams, entity);
+        handleResponse(response);
+
+        restClient.close();
+    }
+
+    @Test
+    void performRequestWithEntityAsync() throws Exception {
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("user", "JamesiWork");
+        jsonObject.addProperty("postDate", "2013-02-01");
+        jsonObject.addProperty("message", "ELK: Elasticsearch Logstash Kibana");
+        HttpEntity entity = new NStringEntity(gson.toJson(jsonObject), ContentType.APPLICATION_JSON);
+
+        builder = Builder.setHttpClientConfigCallback(builder);
+        builder = Builder.setDefaultHeaders(builder);
+        RestClient restClient = builder.build();
+        restClient.performRequestAsync("PUT", "/twitter/doc/6", singletonParams, entity, responseListener);
+    }
+
+    @Test
+    void performRequestWithConsumerFact() throws Exception {
+        builder = Builder.setHttpClientConfigCallback(builder);
+        RestClient restClient = builder.build();
+        // Send a request by providing optional request body,
+        // and the optional factory that is used to create an org.apache.http.nio.protocol.HttpAsyncResponseConsumer callback instance per request attempt.
+        Response response = restClient.performRequest("GET", "/twitter/_search", singletonParams, null, consumerFactory);
+        handleResponse(response);
+
+        restClient.close();
+    }
+
+    @Test
+    void performRequestWithConsumerFactAsync() throws Exception {
+        builder = Builder.setHttpClientConfigCallback(builder);
+        RestClient restClient = builder.build();
+        restClient.performRequestAsync("GET", "/twitter/_search", singletonParams, null, consumerFactory, responseListener);
+    }
+
+    @Test
+    void performRequestWithEntityConsumerFact() throws Exception {
+        String all = "{\n" +
+                "    \"query\" : {\n" +
+                "        \"match_all\" : {}\n" +
+                "    }\n" +
+                "}";
+
+        String match = "{\n" +
+                "    \"query\" : {\n" +
+                "        \"match\" : { \"user\": \"JamesiWork\" }\n" +
+                "    }\n" +
+                "}";
+
+        String range = "{\n" +
+                "    \"query\" : {\n" +
+                "        \"range\" : {\n" +
+                "            \"post_date\" : { \"from\" : \"2017-01-01T00:00:00\", \"to\" : \"2017-09-14T20:00:00\" }\n" +
+                "        }\n" +
+                "    }\n" +
+                "}";
+
+        HttpEntity entity = new NStringEntity(range, ContentType.APPLICATION_JSON);
+
+        builder = Builder.setHttpClientConfigCallback(builder);
+        builder = Builder.setDefaultHeaders(builder);
+        RestClient restClient = builder.build();
+        // Send a request by providing optional request body,
+        // and the optional factory that is used to create an org.apache.http.nio.protocol.HttpAsyncResponseConsumer callback instance per request attempt.
+        Response response = restClient.performRequest("GET", "/twitter/_search", singletonParams, entity, consumerFactory);
+        handleResponse(response);
+
+        restClient.close();
+    }
+
+}
